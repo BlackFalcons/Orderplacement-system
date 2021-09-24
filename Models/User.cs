a5dfcbbae1e9081d028aa67ee17c904daefa909f
Eivind Limstrand <eivindlimstrand@gmail.com>
2021-09-24 11:28:27 +0200

    Add user model

---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Orderplacement_system.Models
+{
+    // Record types have better support for immutable objects. (Cannot modify)
+    // When you compare 2 instances they will be equal if all the properties are the same.
+    public record User
+    {
+        // init so you can modify the property during when it's being constructed but not after it's created.
+        // Guid object useful to generate uniqe ID's
+        public Guid Id { get; init; }
+        public string Name { get; set; }
+        public string Phone{ get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public string Street {  get; set; }
+    }
+}
